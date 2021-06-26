76428af973a9102c334dd9bc7d720395938eb3b8
Alex <grok88@tut.by>
2021-06-26 15:32:52 +0300

    refactor json file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {DataType, Table} from './components/Table/Table';
+import data from './data/data.json'
 
 function App() {
+    const [response, setData] = useState<DataType[] | null>(null);
+
+
+    useEffect(() => {
+
+        // @ts-ignore
+        setData(data);
+
+        // const response = fetch(`./data/data.json`);
+        // response.then(dataJson =>  {
+        //     debugger
+        //     return dataJson.json()
+        // } )
+        //     .then(res => {
+        //         debugger
+        //         return setData(res)
+        //     })
+        //     .catch(err=> console.log(err));
+
+    }, []);
+
     return (
-        <div className="App">
-            Test
-        </div>
+        <>
+            <div className="App">
+                {
+                    response && <Table dataUrl={response}/>
+                }
+            </div>
+        </>
     );
 }
 
---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import classes from './Table.module.css'
+
+type CellType = {
+    text: string
+    cellId: number
+}
+export type DataType = {
+    rowId: number
+    cells: Array<CellType>
+}
+
+type TablePropsType = {
+    dataUrl: Array<DataType>
+}
+
+let headers: Array<string> = ['title1', 'title2', 'title3', 'title4', 'title5', 'title6', 'title7', 'title8', 'title9', 'title10', 'title11', 'title12', 'title13',]
+
+export const Table: React.FC<TablePropsType> = ({dataUrl}): React.ReactElement => {
+    return <table className={classes.table}>
+        <caption>
+            Table example
+        </caption>
+        <thead>
+        <tr>
+            {headers.map((header, i) => <th key={i}>{header}</th>)}
+        </tr>
+        </thead>
+        <tbody>
+        {dataUrl.map(item => (
+            <tr key={item.rowId}>
+                {item.cells.map((cell, i) => {
+                    if (typeof cell.text === 'string' ){
+                        return <td key={cell.cellId}>{cell.text}</td>
+                    } else  return <td key={cell.cellId}>Не строка</td>
+                })}
+            </tr>
+        ))}
+        </tbody>
+    </table>
+}
+
